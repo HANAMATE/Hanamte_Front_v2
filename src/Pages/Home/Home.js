e17476ffd1dfab7606136b8c8eaba8d626169c9a
helloalpaca <jms393497@gmail.com>
2023-08-28 14:26:48 +0900

    init 적용

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -10,6 +10,7 @@ import Footer from "../../components/Layout/Footer";
 import RootLayout from "../../components/Layout/RootLayout";
 import { useNavigate } from "react-router-dom";
 import { fetchBalance } from "../../apis/requests";
+import axios from "axios";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -20,6 +21,37 @@ const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    let localStorageAT = localStorage.getItem("AccessToken");
+    let localStorageRT = localStorage.getItem("RefreshToken");
+
+    async function fetchInit() {
+      await axios
+        .post(
+          process.env.REACT_APP_SERVER_URL + "/users/init",
+          {},
+          { headers: { Authorization: localStorageAT.replace("Bearer ", "") } }
+        )
+        .then((response) => {
+          const isParent = response.data.data.userType === "Parent";
+          console.log(response.data.data.userName);
+          dispatch(
+            authActions.login({
+              loginId: response.data.data.userId,
+              name: response.data.data.userName,
+              userType: isParent,
+              accessToken: localStorageAT,
+              refreshToken: localStorageRT,
+            })
+          );
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .finally((fin) => {
+          console.log("end");
+        });
+    }
+
     async function getbalance() {
       try {
         const response = await fetchBalance({ userId: loginId });
@@ -33,14 +65,21 @@ const Home = () => {
       }
     }
 
-    let localStorageAT = localStorage.getItem("AccessToken");
-    let localStorageRT = localStorage.getItem("RefreshToken");
-
     // console.log(localStorageAT);
+    /*
     if (accessToken === "") {
       navigate("/login");
     } else {
+      fetchInit();
       getbalance();
+    }*/
+
+    if (localStorageAT === null && accessToken === "") {
+      navigate("/login");
+    } else {
+      fetchInit().then(() => {
+        getbalance();
+      });
     }
   }, []);
 
