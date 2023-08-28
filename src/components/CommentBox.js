85ecd69332cf79022fdb0e559468cf084fcd23d2
Ansix1207 <choiansik98@naver.com>
2023-08-28 13:35:54 +0900

    Feat: 거래내역에 모든 데이터 연결, 글 작성 후 지난 시간 적용

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -4,52 +4,28 @@ import Comment from "./Comment";
 import classes from "./CommentBox.module.css";
 import CommentNew from "./CommentNew";
 
-import Profile01 from "../assets/profile01.png";
-import Profile02 from "../assets/profile02.png";
-import Profile03 from "../assets/profile03.png";
-import Profile04 from "../assets/profile04.png";
-import Profile05 from "../assets/profile05.png";
+function getTimeDifferenceString(timestamp) {
+  const now = new Date();
+  const targetTime = new Date(timestamp);
+  const timeDifference = now - targetTime;
 
-const DUMMY = [
-  {
-    id: 1,
-    image: Profile01,
-    name: "권민선",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in neque et diam sagittis vestibulum ac vitae nisl. Morbi lacinia eu urna a sollicitudin. Quisque felis velit, sollicitudin et placerat id, elementum eget odio. Curabitur vitae mi tortor. Nam eu venenatis libero, quis sagittis justo. Cras sed tortor in leo fringilla porttitor aliquam eu quam. Nulla id pharetra ante, id tincidunt sapien.",
-    date: "3시간 전",
-  },
-  {
-    id: 2,
-    image: Profile02,
-    name: "최안식",
-    content: "두번째 댓글",
-    date: "7시간 전",
-  },
-  {
-    id: 3,
-    image: Profile03,
-    name: "김민재",
-    content: "세번째 댓글",
-    date: "1일 전",
-  },
-  {
-    id: 4,
-    image: Profile04,
-    name: "강민경",
-    content: "네번째 댓글",
-    date: "2일 전",
-  },
-  {
-    id: 5,
-    image: Profile05,
-    name: "민새미",
-    content: "다섯번째 댓글",
-    date: "2일 전",
-  },
-];
-
+  if (timeDifference < 60000) { // Less than 1 minute
+    return `${Math.floor(timeDifference / 1000)}초 전`;
+  } else if (timeDifference < 3600000) { // Less than 1 hour
+    return `${Math.floor(timeDifference / 60000)}분 전`;
+  } else if (timeDifference < 86400000) { // Less than 1 day
+    return `${Math.floor(timeDifference / 3600000)}시간 전`;
+  } else if (timeDifference < 604800000) { // Less than 1 week
+    return `${Math.floor(timeDifference / 86400000)}일 전`;
+  } else if (timeDifference < 1209600000) { // Less than 2 weeks
+    return `${Math.floor(timeDifference / 604800000)}주 전`;
+  } else { // 2 weeks or more
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return targetTime.toLocaleDateString("ko-KR", options);
+  }
+}
 const CommentBox = (props) => {
+  const DUMMY = props.comment;
   const moreThanThree = DUMMY.length > 3;
   const [showAllComments, setShowAllComments] = useState(false);
 
@@ -60,11 +36,11 @@ const CommentBox = (props) => {
   return (
     <div className={classes.container}>
       {DUMMY.slice(0, 3).map((each) => (
-        <Comment key={each.id} image={each.image} name={each.name} date={each.date} content={each.content} />
+        <Comment key={each.id} image={each.image} name={each.writerName} date={getTimeDifferenceString(each.createDate)} content={each.commentContent} />
       ))}
       {showAllComments &&
         DUMMY.slice(3).map((each) => (
-          <Comment key={each.id} image={each.image} name={each.name} date={each.date} content={each.content} />
+          <Comment key={each.id} image={each.image} name={each.writerName} date={getTimeDifferenceString(each.createDate)} content={each.commentContent} />
         ))}
       {moreThanThree && (
         <button className={classes.seeMore} onClick={clickSeeMoreHandler}>
