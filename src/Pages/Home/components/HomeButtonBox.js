2a6df7ab16b0b666a0398398dab9e4feca2d10c3
helloalpaca <jms393497@gmail.com>
2023-08-29 22:22:10 +0900

    UserType별 홈화면 수정

--- src/Pages/Home/components/HomeButtonBox.js
+++ src/Pages/Home/components/HomeButtonBox.js
@@ -4,8 +4,10 @@ import { FaPray, FaWallet } from "react-icons/fa";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { RiBankFill } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const HomeButtonBox = (props) => {
+  const { userType } = useSelector((state) => state.auth);
   const navigate = useNavigate();
   const allowanceClickHandler = () => {
     navigate("allowance");
@@ -21,59 +23,80 @@ const HomeButtonBox = (props) => {
   };
   const myMoimClickHandler = () => {
     navigate("moim"); // 이동하고자 하는 페이지의 경로를 넣어줍니다.
-  }
+  };
   const sendClickHandler = () => {
     navigate("/allowance/send");
   };
-  const periodicClickHandler = () => {
-    navigate("/allowance/periodic");
+  const approveClickHandler = () => {
+    navigate("/allowance/parent");
   };
 
   return (
     <div className={classes.container}>
       <div className={classes.buttonContainer}>
-        <HomeButton
-          background="var(--violet2)"
-          icon={<FaWallet size="32" />}
-          title="직접 관리하는"
-          subTitle="내 지갑!"
-          onClick={allowanceClickHandler}
-        />
-        <HomeButton
-          background="var(--blue4)"
-          icon={<FaPray size="32" />}
-          title="용돈 부족하면"
-          subTitle="부탁해봐요!"
-          onClick={askClickHandler}
-        />
-        <HomeButton
-          background="var(--yellow2)"
-          icon={<RiBankFill size="32" />}
-          title="진행 중인"
-          subTitle="대출이 없네요"
-          onClick={loanClickHandler}
-        />
-        <HomeButton
-          background="var(--red1)"
-          icon={<BsFillPeopleFill size="32" />}
-          title="모임통장 리스트"
-          subTitle="보러가기"
-          onClick={myMoimClickHandler}
-          />
-          <HomeButton
-          background="var(--red2)"
-          icon={<BsFillPeopleFill size="32" />}
-          title="내 아이에게"
-          subTitle="용돈주기"
-          onClick={sendClickHandler}
-        />
-        <HomeButton
-          background="var(--violet2)"
-          icon={<FaWallet size="32" />}
-          title="우리 아이"
-          subTitle="정기용돈 설정하기"
-          onClick={periodicClickHandler}
-        />
+        {userType ? (
+          <>
+            <HomeButton
+              background="var(--violet2)"
+              icon={<FaWallet size="32" />}
+              title="직접 관리하는"
+              subTitle="내 지갑!"
+              onClick={allowanceClickHandler}
+            />
+            <HomeButton
+              background="var(--red1)"
+              icon={<BsFillPeopleFill size="32" />}
+              title="모임통장 리스트"
+              subTitle="보러가기"
+              onClick={myMoimClickHandler}
+            />
+            <HomeButton
+              background="var(--red2)"
+              icon={<BsFillPeopleFill size="32" />}
+              title="내 아이에게"
+              subTitle="용돈주기"
+              onClick={sendClickHandler}
+            />
+            <HomeButton
+              background="var(--violet2)"
+              icon={<FaWallet size="32" />}
+              title="아이가"
+              subTitle="용돈을 졸랐어요"
+              onClick={approveClickHandler}
+            />
+          </>
+        ) : (
+          <>
+            <HomeButton
+              background="var(--violet2)"
+              icon={<FaWallet size="32" />}
+              title="직접 관리하는"
+              subTitle="내 지갑!"
+              onClick={allowanceClickHandler}
+            />
+            <HomeButton
+              background="var(--blue4)"
+              icon={<FaPray size="32" />}
+              title="용돈 부족하면"
+              subTitle="부탁해봐요!"
+              onClick={askClickHandler}
+            />
+            <HomeButton
+              background="var(--yellow2)"
+              icon={<RiBankFill size="32" />}
+              title="진행 중인"
+              subTitle="대출이 없네요"
+              onClick={loanClickHandler}
+            />
+            <HomeButton
+              background="var(--red1)"
+              icon={<BsFillPeopleFill size="32" />}
+              title="모임통장 리스트"
+              subTitle="보러가기"
+              onClick={myMoimClickHandler}
+            />
+          </>
+        )}
       </div>
     </div>
   );
